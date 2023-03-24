b4cadf206d994991a1c28674a391389c2190604a
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-03-24 15:58:16 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
+// ==UserScript==
+// @name         Search for genre or size
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @license      MIT
+// @description  Search for Genres or Size on https://fitgirl-repacks.site/
+// @author       TheEmptynessProject
+// @match        https://fitgirl-repacks.site/*
+// @icon         https://github.com/TheEmptynessProject/fitgirl-search-genreORsize
+// ==/UserScript==
 function GET_storage_underX(sizevar) {
-    const url = JSON.parse(localStorage.getItem("urlArray")) || [];
-    const retri = JSON.parse(localStorage.getItem("sizeArray")) || [];
-    if (retri == []) {
-     return null;
-    }
-		for (let i = 0; i < retri.length; i++) {
-            if (retri[i] <= sizevar){
-                console.log(`${retri[i]}: ${url[i]}\n`)
-            }
+	const url = JSON.parse(localStorage.getItem("urlArray")) || [];
+	const retri = JSON.parse(localStorage.getItem("sizeArray")) || [];
+	if (retri == []) {
+		return null;
+	}
+	for (let i = 0; i < retri.length; i++) {
+		if (retri[i] <= sizevar) {
+			console.log(`${retri[i]}: ${url[i]}\n`)
 		}
+	}
 }
+
 function convertToMB(sizevar) {
 	const regex = /(\d+(\.\d+)?)\s*(GB|MB)/i;
 	const match = sizevar.match(regex);
@@ -32,14 +43,14 @@ function convertToMB(sizevar) {
 	'use strict';
 	var myArray = JSON.parse(localStorage.getItem('urlArray')) || []
 	var sizeArray = JSON.parse(localStorage.getItem('sizeArray')) || []
-	const genresWanted = [];
-	const maxSize = convertToMB('5 GB')
+	const genresWanted = ["Open World"]; //Change to wanted genres, leave empty to search for size
+	const maxSize = convertToMB('5 GB') //Change to search for wanted size
 	const packsizeBool = true; //Change to check either repack size (true) or real size (false)
 	const posts = document.getElementsByClassName("post");
-	let stopScript = false; // flag to track whether the button has been clicked
+	let stopScript = false; // flag to track whether the button Stop script has been clicked
 	for (const post of posts) {
 		if (stopScript) {
-			break; // stop script execution if button has been clicked
+			break;
 		}
 		const content = post.getElementsByClassName("entry-content");
 		for (const elem of content) {
@@ -50,40 +61,40 @@ function convertToMB(sizevar) {
 					const child = post.querySelector('.entry-content > p').querySelectorAll('strong');;
 					var realsize;
 					var packsize;
-                    for (const xxx of child) {
+					for (const xxx of child) {
 						if (xxx.previousSibling.textContent.toLowerCase().includes('original')) {
 							realsize = convertToMB(xxx.innerHTML)
 						} else if (xxx.previousSibling.textContent.toLowerCase().includes('pack')) {
 							packsize = convertToMB(xxx.innerHTML)
 						}
-                    }
-						if (genresWanted.length == 0) {
-							if (packsizeBool) {
-								if (packsize <= maxSize) {
-									sizeArray.push(packsize)
-									myArray.push(link);
-									localStorage.setItem('urlArray', JSON.stringify(myArray));
-									localStorage.setItem('sizeArray', JSON.stringify(sizeArray))
-								}
-							} else {
-								if (realsize <= maxSize) {
-									sizeArray.push(realsize)
-									myArray.push(link);
-									localStorage.setItem('urlArray', JSON.stringify(myArray));
-									localStorage.setItem('sizeArray', JSON.stringify(sizeArray))
-								}
+					}
+					if (genresWanted.length == 0) {
+						if (packsizeBool) {
+							if (packsize <= maxSize) {
+								sizeArray.push(packsize)
+								myArray.push(link);
+								localStorage.setItem('urlArray', JSON.stringify(myArray));
+								localStorage.setItem('sizeArray', JSON.stringify(sizeArray))
 							}
 						} else {
-							for (const genre of genresWanted) {
-								if (temp[3].outerText.toLowerCase().includes(genre.toLowerCase())) {
-									sizeArray.push(`(${realsize}:${packsize})`)
-									myArray.push(link);
-									localStorage.setItem('urlArray', JSON.stringify(myArray));
-									localStorage.setItem('sizeArray', JSON.stringify(sizeArray))
-								} else {
-									post.style.display = "none";
-								}
+							if (realsize <= maxSize) {
+								sizeArray.push(realsize)
+								myArray.push(link);
+								localStorage.setItem('urlArray', JSON.stringify(myArray));
+								localStorage.setItem('sizeArray', JSON.stringify(sizeArray))
 							}
+						}
+					} else {
+						for (const genre of genresWanted) {
+							if (temp[3].outerText.toLowerCase().includes(genre.toLowerCase())) {
+								sizeArray.push(`(${realsize}:${packsize})`)
+								myArray.push(link);
+								localStorage.setItem('urlArray', JSON.stringify(myArray));
+								localStorage.setItem('sizeArray', JSON.stringify(sizeArray))
+							} else {
+								post.style.display = "none";
+							}
+						}
 					}
 				}
 			}
@@ -98,30 +109,30 @@ function convertToMB(sizevar) {
 			}
 		}
 	}
-let underX = document.createElement('button');
-    let underInput = document.createElement('input');
-    let userInput
-    underInput.style.borderBottom = '5px solid #000';
-    underInput.style.borderRadius = '15px';
-    underInput.className = 'hoverEffect'
-underInput.setAttribute("type", "text");
-underInput.style.color = 'gold'
-underInput.style.backgroundColor = '#363636'
-//underInput.style.zIndex = '99'
-underInput.style.position = 'fixed'
-underInput.style.bottom = '20px'
-underInput.style.width = '250px'
-underInput.style.height = '150px'
-    underInput.style.left = '570px';
-underInput.addEventListener('change', function() {
-  userInput = underInput.value;
-  console.log(userInput);
-});
+	let underX = document.createElement('button');
+	let underInput = document.createElement('input');
+	let userInput
+	underInput.style.borderBottom = '5px solid #000';
+	underInput.style.borderRadius = '15px';
+	underInput.className = 'hoverEffect'
+	underInput.setAttribute("type", "text");
+	underInput.style.color = 'gold'
+	underInput.style.backgroundColor = '#363636'
+	//underInput.style.zIndex = '99'
+	underInput.style.position = 'fixed'
+	underInput.style.bottom = '20px'
+	underInput.style.width = '250px'
+	underInput.style.height = '150px'
+	underInput.style.left = '570px';
+	underInput.addEventListener('change', function() {
+		userInput = underInput.value;
+		console.log(userInput);
+	});
 	underX.innerHTML = 'Get storage values lower than:';
 	underX.style.color = 'white';
-    underX.className = 'hoverEffect'
-    underX.style.borderBottom = '5px solid #000';
-    underX.style.borderRadius = '15px';
+	underX.className = 'hoverEffect'
+	underX.style.borderBottom = '5px solid #000';
+	underX.style.borderRadius = '15px';
 	underX.style.backgroundColor = 'purple';
 	//underX.style.zIndex = '99';
 	underX.style.position = 'fixed';
@@ -130,21 +141,21 @@ underInput.addEventListener('change', function() {
 	underX.style.height = '150px';
 	underX.style.left = '320px';
 	underX.addEventListener('click', function() {
-GET_storage_underX(userInput)
+		GET_storage_underX(userInput)
 	});
 	let button = document.createElement('button');
 	button.innerHTML = 'Stop Script';
-    button.style.borderBottom = '10px solid #000';
-    button.style.borderRadius = '15px';
+	button.style.borderBottom = '10px solid #000';
+	button.style.borderRadius = '15px';
 	button.style.color = 'white';
 	button.style.backgroundColor = 'purple';
-//	button.style.zIndex = '99';
+	//	button.style.zIndex = '99';
 	button.style.position = 'fixed';
 	button.style.bottom = '20px';
 	button.style.width = '300px';
 	button.style.height = '200px';
 	button.style.left = '20px';
-    button.className = 'hoverEffect'
+	button.className = 'hoverEffect'
 	button.addEventListener('click', function() {
 		stopScript = true;
 		const retrievedArray = JSON.parse(localStorage.getItem("urlArray")) || JSON.parse(localStorage.getItem("sizeArray")) || [];
@@ -155,11 +166,11 @@ GET_storage_underX(userInput)
 	let continueB = document.createElement('button');
 	continueB.innerHTML = 'Continue Script';
 	continueB.style.color = 'white';
-    continueB.style.borderBottom = '10px solid #000';
-    continueB.style.borderRadius = '15px';
+	continueB.style.borderBottom = '10px solid #000';
+	continueB.style.borderRadius = '15px';
 	continueB.style.backgroundColor = 'blue';
-//	continueB.style.zIndex = '99';
-    continueB.className = 'hoverEffect'
+	//	continueB.style.zIndex = '99';
+	continueB.className = 'hoverEffect'
 	continueB.style.position = 'fixed';
 	continueB.style.bottom = '20px';
 	continueB.style.width = '300px';
@@ -173,27 +184,27 @@ GET_storage_underX(userInput)
 	clear.style.color = 'white';
 	clear.style.backgroundColor = 'red';
 	//clear.style.zIndex = '99';
-    clear.style.borderBottom = '10px solid #000';
-    clear.style.borderRadius = '15px';
+	clear.style.borderBottom = '10px solid #000';
+	clear.style.borderRadius = '15px';
 	clear.style.position = 'fixed';
 	clear.style.bottom = '20px';
 	clear.style.width = '300px';
 	clear.style.height = '200px';
-    clear.className = 'hoverEffect'
+	clear.className = 'hoverEffect'
 	clear.style.right = '320px';
 	clear.addEventListener('click', function() {
 		localStorage.removeItem('urlArray');
-        localStorage.removeItem('sizeArray');
+		localStorage.removeItem('sizeArray');
 		alert("Cleared Storage")
 	});
 	document.body.appendChild(button);
 	document.body.appendChild(continueB);
 	document.body.appendChild(clear);
-    document.body.appendChild(underX);
+	document.body.appendChild(underX);
 	document.body.appendChild(underInput);
 
-var styleElement = document.createElement("style");
-styleElement.innerHTML = `
+	var styleElement = document.createElement("style");
+	styleElement.innerHTML = `
 .hoverEffect {
   border: 1px solid #000;
   transition: all 0.3s ease;
@@ -207,7 +218,7 @@ styleElement.innerHTML = `
   z-index: 3;
 }
 `
-document.head.appendChild(styleElement);
+	document.head.appendChild(styleElement);
 
 	const currentPage = parseInt(document.location.href.match(/\/page\/(\d+)\//)?.[1]) || 1;
 	if (currentPage < maxPageNum) {
